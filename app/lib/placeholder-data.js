4d7415a8b0ce20b3e26efc64b2a0cf1b3e0f5f0d
hitech37 <dennislee91@outlook.com>
2024-06-19 10:41:10 -0400

    first commit

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,14 @@
+const invoices = [
+    {
+        customer_id: customers[0].id,
+        amount: 15795,
+        status: 'pending',
+        data: '2024-06-19'
+    },
+    {
+        customer_id: customers[1].id,
+        amount: 15343,
+        status: 'pending',
+        data: '2024-02-11'
+    },
+]
